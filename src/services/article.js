0aaa4c43421ddddabda0e6f6c125589547203fdf
Revanth kumar S <revanthsureshlearn@gmail.com>
2024-12-15 10:52:42 +0530

    initial upload

---
+++ src/services/article.js
@@ -0,0 +1,36 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+export const articleApi = createApi({
+  reducerPath: "articleApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://article-extractor-and-summarizer.p.rapidapi.com/",
+    prepareHeaders: (headers) => {
+      headers.set("X-RapidAPI-Key", rapidApiKey);
+      headers.set(
+        "X-RapidAPI-Host",
+        "article-extractor-and-summarizer.p.rapidapi.com"
+      );
+      console.log(headers.get("X-RateLimit-Requests-Remaining"));
+      return headers;
+    },
+    fetchFn: async (...args) => {
+      const response = await fetch(...args);
+      const remainingLimit = response.headers.get(
+        "X-RateLimit-Requests-Remaining"
+      );
+      console.log("X-RateLimit-Requests-Remaining:", remainingLimit);
+
+      return response;
+    },
+  }),
+  endpoints: (builder) => ({
+    getSummary: builder.query({
+      query: (params) =>
+        `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+    }),
+  }),
+});
+
+export const { useLazyGetSummaryQuery } = articleApi;
